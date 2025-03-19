a4f4d909645ecfeed8018e3c3024d5f0ee76b3b7
Echo <github@3kh0.net>
2025-03-19 12:11:42 -0400

    dont spam the api after we done

--- pages/app/index.vue
+++ pages/app/index.vue
@@ -102,6 +102,10 @@ onMounted(async () => {
   }, 30000);
 });
 
+onUnmounted(() => {
+  clearInterval();
+});
+
 useHead({
   title: "HCBScan",
   meta: [
@@ -171,7 +175,7 @@ useHead({
           placeholder="Search for organizations on HCB..."
         />
         <div
-          v-if="isFocused && !gettingResults"
+          v-if="isFocused && !gettingResults && !query"
           class="absolute z-10 w-full mt-1 bg-zinc-800 rounded-lg shadow-lg"
         >
           <div class="px-4 py-2 text-zinc-400">
