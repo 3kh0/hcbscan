b28170068bbc2d0b11e9e2da63ae64c82c7ccf09
Echo <github@3kh0.net>
2026-03-04 23:44:00 -0500

    highlight missing receipts

--- app/pages/app/org/[id]/index.vue
+++ app/pages/app/org/[id]/index.vue
@@ -515,12 +515,71 @@
                   {{ new Date(txn.date).toLocaleDateString() }}
                 </td>
                 <td class="py-2 px-4 border-b border-zinc-700">
-                  <NuxtLink
-                    :to="`/app/txn/${txn.id}`"
-                    class="text-blue-400 hover:underline"
-                  >
-                    {{ txn.memo }}
-                  </NuxtLink>
+                  <div class="flex items-center gap-2">
+                    <NuxtLink
+                      :to="`/app/txn/${txn.id}`"
+                      class="text-blue-400 hover:underline"
+                    >
+                      {{ txn.memo || "No memo" }}
+                    </NuxtLink>
+                    <span
+                      v-if="txn.receipts?.missing"
+                      class="inline-flex items-center gap-0.5 text-xs text-yellow-400 animate-pulse"
+                      title="Missing receipt"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-3.5 w-3.5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                      Missing receipt!
+                    </span>
+                    <span
+                      v-else-if="txn.receipts?.count > 0"
+                      class="inline-flex items-center gap-0.5 text-xs text-zinc-400"
+                      title="Receipts"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-3.5 w-3.5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                      {{ txn.receipts.count }}
+                    </span>
+                    <span
+                      v-if="txn.comments?.count > 0"
+                      class="inline-flex items-center gap-0.5 text-xs text-zinc-400"
+                      title="Comments"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-3.5 w-3.5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                      {{ txn.comments.count }}
+                    </span>
+                  </div>
                 </td>
                 <td class="py-2 px-4 border-b border-zinc-700">
                   {{ fixMoney(txn.amount_cents) }}
